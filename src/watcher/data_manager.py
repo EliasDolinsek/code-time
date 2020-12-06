34fc14ae7d3b1e9a92b290c1e9833ca7679d67ef
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-12-06 11:43:17 +0100

    Initial statistics browsing

--- src/watcher/data_manager.py
+++ src/watcher/data_manager.py
@@ -1,12 +1,17 @@
 import json as json_parser
 import os
+from os import listdir
 
 CONFIG_FILE = "config.json"
 DATA_DIRECTORY = "data/"
 
 
+def get_data_folder_path():
+    return os.path.join(os.path.dirname(__file__), DATA_DIRECTORY)
+
+
 def get_file_path_for_data_file(month_str: str, year_str: str):
-    return os.path.join(os.path.dirname(__file__), DATA_DIRECTORY, f"{month_str}_{year_str}.json")
+    return os.path.join(get_data_folder_path(), f"{month_str}_{year_str}.json")
 
 
 def read_month_data(month_str: str, year_str: str) -> dict:
@@ -35,3 +40,31 @@ def read_config() -> dict:
 def write_config(config: dict):
     with open(CONFIG_FILE, "w") as file:
         file.write(json_parser.dumps(config))
+
+
+def get_year_of_data_file_name(name: str):
+    month_removed = name[name.index("_") + 1:]
+    return month_removed[:month_removed.index(".")]
+
+
+def get_available_years() -> list:
+    files = listdir(get_data_folder_path())
+    years = []
+    for file in files:
+        year = get_year_of_data_file_name(file)
+        if year not in years:
+            years.append(year)
+
+    years.sort()
+    return years
+
+
+def get_available_months(year: int) -> list:
+    files = listdir(get_data_folder_path())
+    months = []
+    for file in files:
+        if get_year_of_data_file_name(file) == year:
+            months.append(int(file[:file.index("_")]))
+
+    months.sort()
+    return months
--- src/watcher/tray_handler.py
+++ src/watcher/tray_handler.py
@@ -1,5 +1,9 @@
+import datetime
+
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+
+from src.watcher.data_manager import get_available_years, get_available_months, read_month_data
 
 TEXT_PAUSE = "Pause tracking"
 TEXT_CONTINUE = "Continue tracking"
@@ -31,17 +35,50 @@ class TrayHandler:
         self.action_pause_continue.triggered.connect(lambda x: self._on_pause_continue(self.provided_on_pause_continue))
 
         menu.addAction(self.action_pause_continue)
+        self.add_statistics_to_menu(menu)
         menu.addAction(self.action_quit)
 
         tray.setContextMenu(menu)
         self.app.exec_()
 
-    def _on_quit(self, on_quit):
-        on_quit()
-        self.app.quit()
+    def add_days_statistics_to_menu(self, menu, year, month):
+        month_data = read_month_data(month, year)
+        for day in month_data["activity"]:
+            day_menu = menu.addMenu(str(day))
+            self.add_statistic_actions_to_menu(day_menu)
 
-    def _on_pause_continue(self, on_pause_continue):
-        if on_pause_continue():
-            self.action_pause_continue.setText(TEXT_CONTINUE)
-        else:
-            self.action_pause_continue.setText(TEXT_PAUSE)
+    def add_months_statistics_to_menu(self, menu, year):
+        months = get_available_months(year)
+        for month in months:
+            month_str = datetime.date(2000, month, 1).strftime('%B')
+            current_month_menu = menu.addMenu(month_str)
+            self.add_days_statistics_to_menu(current_month_menu, year, month)
+
+    def add_years_statistics_to_menu(self, menu):
+        years = get_available_years()
+        for year in years:
+            current_year_menu = menu.addMenu(year)
+            self.add_months_statistics_to_menu(current_year_menu, year)
+
+    def add_statistics_to_menu(self, menu):
+        statistics_menu = menu.addMenu("Statistics")
+        today_menu = statistics_menu.addMenu("Today")
+
+        self.add_statistic_actions_to_menu(today_menu)
+        self.add_years_statistics_to_menu(statistics_menu)
+
+    def add_statistic_actions_to_menu(self, menu):
+        menu.addAction("Show")
+        menu.addAction("Export as PNG")
+
+
+def _on_quit(self, on_quit):
+    on_quit()
+    self.app.quit()
+
+
+def _on_pause_continue(self, on_pause_continue):
+    if on_pause_continue():
+        self.action_pause_continue.setText(TEXT_CONTINUE)
+    else:
+        self.action_pause_continue.setText(TEXT_PAUSE)
