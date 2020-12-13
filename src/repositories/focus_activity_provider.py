4f5201c653976b6c4f26c1e7f6ea33b531757cc4
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-12-13 11:30:28 +0100

    Make FocusActivityProvider class abstract

--- src/repositories/focus_activity_provider.py
+++ src/repositories/focus_activity_provider.py
@@ -1,10 +1,13 @@
 import sys
+from abc import ABC, abstractmethod
 
 if sys.platform in ['Mac', 'darwin', 'os2', 'os2emx']:
     from AppKit import NSWorkspace
 
 
-class FocusActivityProvider:
+class FocusActivityProvider(ABC):
+
+    @abstractmethod
     def get_activity_name(self) -> str:
         """Get name of the currently focused activity"""
         pass
