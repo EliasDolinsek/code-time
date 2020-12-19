a6ce60ef6e84828f1e66d65cb5c3d9dd0f0fdfa1
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-12-19 11:56:28 +0100

    User image display

--- src/presentation/settings/settings_dialog.py
+++ src/presentation/settings/settings_dialog.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QDialog, QVBoxLayout, QTabWidget, QWidget
 
 from src.presentation.settings.theme_settings_widget import ThemeSettingsWidget
+from src.presentation.settings.user_image_widget import UserImageWidget
 from src.repositories.code_time_data_repository import CodeTimeDataRepository
 
 
@@ -14,12 +15,11 @@ class SettingsDialog(QDialog):
         self.tabs = QTabWidget()
 
         # Tabs
-        self.user_image_tab = QWidget()
         self.autostart_tab = QWidget()
         self.activities_tab = QWidget()
 
         self.tabs.addTab(ThemeSettingsWidget(self.data_repository), "Theme")
-        self.tabs.addTab(self.user_image_tab, "User Image")
+        self.tabs.addTab(UserImageWidget(self.data_repository), "User Image")
         self.tabs.addTab(self.autostart_tab, "Autostart")
         self.tabs.addTab(self.activities_tab, "Activities")
 
--- src/presentation/settings/user_image_widget.py
+++ src/presentation/settings/user_image_widget.py
@@ -1,9 +1,41 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout, QPushButton
+
+from src.repositories.code_time_data_repository import CodeTimeDataRepository
 
 
 class UserImageWidget(QWidget):
+    IMAGE_WIDTH = 256
 
-    def __init__(self):
+    def __init__(self, data_repository: CodeTimeDataRepository):
         super().__init__()
-        self.layout = QVBoxLayout()
+        self.data_repository = data_repository
 
+        self.layout = QVBoxLayout()
+        self.layout.setAlignment(Qt.AlignCenter)
+
+        self.image_label = QLabel()
+
+        image_pixmap = self.get_user_image_pixmap()
+        image_pixmap = image_pixmap.scaledToWidth(self.IMAGE_WIDTH)
+        self.image_label.setPixmap(image_pixmap)
+
+        self.layout.addWidget(self.image_label)
+        self.layout.addLayout(self.get_buttons_layout())
+
+        self.setLayout(self.layout)
+
+    def get_buttons_layout(self):
+        layout = QHBoxLayout()
+
+        btn_change = QPushButton("CHANGE")
+        btn_remove = QPushButton("REMOVE")
+
+        layout.addWidget(btn_change)
+        layout.addWidget(btn_remove)
+
+        return layout
+
+    def get_user_image_pixmap(self):
+        return QPixmap(self.data_repository.get_config()["user_image"])
