c5cfaaf61b922ec07138c1b7b742b5b0204b883a
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-12-23 16:22:45 +0100

    Mac autostart use case

---
+++ src/use_cases/autostart.py
@@ -0,0 +1,41 @@
+import os
+import plistlib
+from abc import ABC, abstractmethod
+
+
+class Autostart(ABC):
+
+    @abstractmethod
+    def enable(self, name: str, options: dict = None):
+        pass
+
+    @abstractmethod
+    def disable(self, name: str):
+        pass
+
+
+class MacAutostart(Autostart):
+
+    def __init__(self, base_path="/System/Library/LaunchAgents"):
+        self.base_path = base_path
+
+    def enable(self, name: str, options: dict = None):
+        if options is None:
+            raise ValueError("options must not be None")
+        if "Label" not in options:
+            raise ValueError("'Label' must be specified in options")
+        if "ProgramArguments" not in options:
+            raise ValueError("'ProgramArguments' must be specified in options")
+
+        with open(self.get_path_for_name(name), "wb") as file:
+            plistlib.dump(options, file)
+
+    def disable(self, name: str):
+        path = self.get_path_for_name(name)
+        if os.path.exists(path):
+            os.remove(path)
+        else:
+            raise FileNotFoundError(f"Could not find file {path}")
+
+    def get_path_for_name(self, name):
+        return f"{self.base_path}/{name}.plist"
