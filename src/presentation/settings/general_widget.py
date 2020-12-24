62dcc2e3dcd8a69d12febceaecbb45189ab55c4b
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-12-24 13:33:26 +0100

    Connect username field to data repository

--- src/presentation/settings/general_widget.py
+++ src/presentation/settings/general_widget.py
@@ -1,10 +1,13 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
 
+from src.repositories.code_time_data_repository import CodeTimeDataRepository
+
 
 class GeneralWidget(QWidget):
 
-    def __init__(self):
+    def __init__(self, data_repository: CodeTimeDataRepository):
         super().__init__()
+        self.data_repository = data_repository
         self.layout = QVBoxLayout()
 
         self.username_label = QLabel()
@@ -12,6 +15,7 @@ class GeneralWidget(QWidget):
 
         self.username_field = QLineEdit(self)
         self.username_field.textChanged[str].connect(self.on_username_changed)
+        self.display_username()
 
         self.btn_reset_settings = QPushButton()
         self.btn_reset_settings.setText("RESET SETTINGS")
@@ -23,5 +27,8 @@ class GeneralWidget(QWidget):
 
         self.setLayout(self.layout)
 
+    def display_username(self):
+        self.username_field.setText(self.data_repository.get_setting("username"))
+
     def on_username_changed(self, value):
-        print(value)
+        self.data_repository.update_setting("username", value)
